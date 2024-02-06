684186cc6ec73881bcbc00ce92e3081b026a4ee3
Nicoló Montaspro <127590621+Nicomont1906@users.noreply.github.com>
2024-02-06 16:56:09 +0100

    Add files via upload

---
+++ montaspro.nicolo.4i.rubrica/MainWindow.xaml.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace montaspro.nicolo._4i.rubrica
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window 
+    { 
+        List<Persona> Persone = new List<Persona>();
+        List<Contatto> Contatti = new List<Contatto>();
+        public MainWindow()
+        {
+            InitializeComponent();
+        } 
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                StreamReader fin = new StreamReader("persone.csv");
+                fin.ReadLine();
+                while (!fin.EndOfStream)
+                {
+                    string rigaP = fin.ReadLine();
+                    Persona persona = new Persona(rigaP);
+                    Persone.Add(persona);
+                }
+                dgPersone.ItemsSource = Persone;
+
+                statusbar.Text = $"Ho letto dal file {Persone.Count} righe";
+
+                StreamReader fine = new StreamReader("contatti.csv");
+                fine.ReadLine();
+                while (!fine.EndOfStream)
+                {
+                    string rigaC = fine.ReadLine();
+                    Contatto contatto = new Contatto(rigaC);
+                    Contatti.Add(contatto);
+                }
+                dgContatti.ItemsSource = Contatti;
+            }
+
+            catch (Exception ex)
+            {
+                MessageBox.Show("Errore: " + ex.Message);
+            }
+        }
+        private void dgDati_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            Persona p = e.AddedItems[0] as Persona;
+            statusbar.Text = $"{p.cognome}";
+
+            List<Contatto> contattiFiltrati = new List<Contatto>();
+            foreach (var item in Contatti)
+                if (item.IdPersona == p.IdPersona)
+                    contattiFiltrati.Add(item);
+
+            dgContatti.ItemsSource = contattiFiltrati;
+
+        }
+    }
+}
---
+++ montaspro.nicolo.4i.rubrica/Persona.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace montaspro.nicolo._4i.rubrica
+{
+    class Persona
+    {
+        public int IdPersona { get; set; }
+        public string nome { get; set; }
+        public string cognome { get; set; }
+
+        public Persona(){}
+
+        public Persona(string riga) 
+        {
+            string[] campi = riga.Split(';');
+
+            int Id = 0;
+            int.TryParse(campi[0], out Id);
+            IdPersona = Id;
+
+            this.nome = campi[1];
+            this.cognome = campi[2];
+        }
+    }
+}
