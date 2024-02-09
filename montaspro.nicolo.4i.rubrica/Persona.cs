922759080d8f3f54ccefb251b18e38a1792e3921
Nicoló Montaspro <127590621+Nicomont1906@users.noreply.github.com>
2024-02-09 11:28:33 +0100

    Update Persona.cs

--- montaspro.nicolo.4i.rubrica/Persona.cs
+++ montaspro.nicolo.4i.rubrica/Persona.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -17,6 +17,11 @@ namespace montaspro.nicolo._4i.rubrica
         public Persona(string riga) 
         {
             string[] campi = riga.Split(';');
+
+            if (campi.Count() != 3)
+            {
+                throw new Exception("Le righe del file contatti.csv, devono essere di tre colonne"); 
+            }
 
             int Id = 0;
             int.TryParse(campi[0], out Id);
