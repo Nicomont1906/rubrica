4935356a67dcbc85ff732ad35077a2b9ed848837
Nicoló Montaspro <127590621+Nicomont1906@users.noreply.github.com>
2024-02-14 10:33:48 +0100

    Update Contatto.cs

--- montaspro.nicolo.4i.rubrica/Contatto.cs
+++ montaspro.nicolo.4i.rubrica/Contatto.cs
@@ -1,13 +1,16 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
 namespace montaspro.nicolo._4i.rubrica
 {
     public enum TipoContatto { nessuno, Email, Telefono, Web, Instagram, Facebook, Cellulare }
-    class Contatto
+   public class Contatto
     {
         public int IdPersona { get; set; }
         public TipoContatto Tipo { get; set; }
@@ -20,6 +23,7 @@ namespace montaspro.nicolo._4i.rubrica
         public Contatto(string riga)
         {
             string[] campi = riga.Split(';');
+           
 
         
 
@@ -32,6 +36,53 @@ namespace montaspro.nicolo._4i.rubrica
             this.Tipo = c;
 
             this.Val = campi[2];
+        }
+
+        public static Contatto MakeContatto(string riga)
+        {
+            string[] campi = riga.Split(';');
+
+
+            TipoContatto c;
+            Enum.TryParse(campi[1], out c);
+            
+            switch(c)
+            {
+                case TipoContatto.Email:
+                    return new ContattoEmail(riga);
+                    break;
+            }
+            return new Contatto(riga);
+        }
+    }
+
+    public class ContattoEmail: Contatto
+    {
+        public ContattoEmail() { }
+
+        public ContattoEmail(string riga)
+            : base(riga)
+        {
+
+        }
+    }
+
+    public class Contatti : List<Contatto>
+    {
+        public Contatti() { }
+
+        public Contatti(string nomeFile)
+        {
+
+            StreamReader fine = new StreamReader(nomeFile);
+            fine.ReadLine();
+            while (!fine.EndOfStream)
+
+               Add( Contatto.MakeContatto(fine.ReadLine()));
+
+            fine.Close();
+
+
         }
     }
 }
