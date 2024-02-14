912633050546902281edc68590b8e50c5bbe905f
Nicoló Montaspro <127590621+Nicomont1906@users.noreply.github.com>
2024-02-14 10:35:12 +0100

    Update Persona.cs

--- montaspro.nicolo.4i.rubrica/Persona.cs
+++ montaspro.nicolo.4i.rubrica/Persona.cs
@@ -3,10 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace montaspro.nicolo._4i.rubrica
 {
-    class Persona
+   public class Persona
     {
         public int IdPersona { get; set; }
         public string nome { get; set; }
@@ -29,6 +30,22 @@ namespace montaspro.nicolo._4i.rubrica
 
             this.nome = campi[1];
             this.cognome = campi[2];
+        }
+    }
+
+    public class Persone : List<Persona>
+    {
+        public Persone() { }
+
+        public Persone (string nomeFile)
+        {
+            StreamReader fin = new StreamReader(nomeFile);
+            fin.ReadLine();
+            while (!fin.EndOfStream)
+            
+                Add(new Persona(fin.ReadLine()));
+
+            fin.Close();
         }
     }
 }
